a09d54b79f533e176bc55a30cc7bacec01dc90b2
Zeynep <zeynepqtuncel@gmail.com>
2026-07-20 21:48:22 +0300

    Alan Hesaplama projesi yapıldı

---
+++ 01_Temel_islemler/03-UcgenAlanHesaplama/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Taban Uzunluğunu Giriniz: ");
+double t= Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Yükseklik Giriniz: ");
+double h= Convert.ToDouble(Console.ReadLine());
+
+double alan = (t*h)/2;
+Console.WriteLine($"Üçgen alanı: {alan}");
+
