5c9b4cbdc92bff0422ec157ffc49895e4fc4f393
Zeynep <zeynepqtuncel@gmail.com>
2026-07-20 21:23:04 +0300

    Kullanıcı Girişi projesi yapıldı

---
+++ 01_Temel_islemler/02-KullaniciGirdisi/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Adınızı Giriniz: ");
+string isim = Console.ReadLine();
+Console.Write("Soyadınızı Giriniz: ");
+string soyisim = Console.ReadLine();
+Console.Write("Yaşınızı Giriniz: ");
+int yas =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Hoş Geldiniz {isim} {soyisim}. {yas} yaşındasınız");
+Console.ReadKey();
