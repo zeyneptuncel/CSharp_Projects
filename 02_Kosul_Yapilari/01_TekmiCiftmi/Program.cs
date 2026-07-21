ae9675750fcec5bd13eacfb6bc8162dd6d34c3e1
Zeynep <zeynepqtuncel@gmail.com>
2026-07-21 20:27:34 +0300

    TekmiÇiftmi Projesi eklendi

---
+++ 02_Kosul_Yapilari/01_TekmiCiftmi/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Bir sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+if (sayi%2==1)
+{
+    Console.WriteLine($"{sayi} sayısı tek sayıdır.");
+}
+else
+{
+    Console.WriteLine($"{sayi} sayısı çift sayıdır.");
+}
