35d41665925087d421ce1ba2c15cabb8a30f56ee
Zeynep <zeynepqtuncel@gmail.com>
2026-07-21 21:35:45 +0300

    Vize Final Hesaplama projesi yapıldı

---
+++ 02_Kosul_Yapilari/02_VizeFinalHesaplama/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Vize notunuzu giriniz: ");
+double vize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Final notunuzu giriniz: ");
+double final = Convert.ToInt32(Console.ReadLine());
+Console.Write("Geçme notunuzu giriniz: ");
+double gnot = Convert.ToInt32(Console.ReadLine());
+double not = (vize*0.6) + (final*0.4);
+
+if (not>=gnot)
+{
+    Console.WriteLine($"Tebrikler, dersi geçtiniz. Notunuz: {not}");
+}
+else
+{
+   Console.WriteLine($"Maalesef dersten kaldınız. Notunuz: {not}"); 
+}
