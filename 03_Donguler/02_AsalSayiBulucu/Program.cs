ef007950f7b565382f2e69d6cb0b884145f4ecee
Zeynep <zeynepqtuncel@gmail.com>
2026-07-22 17:49:14 +0300

    Asal sayı bulma projesi yapıldı

---
+++ 03_Donguler/02_AsalSayiBulucu/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Bir sayı giriniz: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool asalmi=true;
+if (number<2)
+{
+    asalmi=false;
+    System.Console.WriteLine("Sayı asal değil");
+}
+
+int i = 2;
+while (i<number)
+{
+    if (number%i == 0)
+    {
+        Console.WriteLine("Sayı asal değil");
+        asalmi=false;
+        break;
+    }
+    i++;
+}
+if (asalmi==true)
+{
+    System.Console.WriteLine("Bu bir asal sayıdır.");
+}
