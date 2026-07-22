b0c918d096308253c605081951699d60cd6dc9f4
Zeynep <zeynepqtuncel@gmail.com>
2026-07-22 16:45:30 +0300

    Kullanıcı Girişi projesi yapıldı

---
+++ 02_Kosul_Yapilari/03_KullaniciGirisi/Program.cs
@@ -0,0 +1,19 @@
+﻿string k_adi = "admin";
+string sifre = "12345";
+Console.Write("Kullanıcı adınızı giriniz: ");
+string k= Console.ReadLine();
+Console.Write("Şifrenizi giriniz: ");
+string s= Console.ReadLine();
+
+if (k_adi==k && sifre==s)
+{
+    Console.WriteLine("Giriş Başarılı");
+}
+else if (k_adi==k && sifre!=s)
+{
+    Console.WriteLine("Şifre yanlış.\nGiriş Yapılamadı.");
+}
+else
+{
+    Console.WriteLine("Hata! Kullanıcı adı veya şifre yanlış.");
+}
