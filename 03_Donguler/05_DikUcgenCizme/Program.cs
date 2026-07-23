1ea0e10c08f45e16e1b55453cb3bcfa01d7ed71c
Zeynep <zeynepqtuncel@gmail.com>
2026-07-23 15:06:31 +0300

    Üçgen çizme projesi yapıldı

---
+++ 03_Donguler/05_DikUcgenCizme/Program.cs
@@ -0,0 +1,20 @@
+﻿// girilen kenar sayısına göre bir ikizkenar dik üçgen çizen program
+
+Console.Write("Kenar uzunluğunu giriniz: ");
+int k = Convert.ToInt32(Console.ReadLine());
+if (k<0)
+{
+    System.Console.WriteLine("HATA. Negatif değer geçersizdir.");
+}
+else
+{
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            System.Console.Write("*");
+        }
+        System.Console.WriteLine();
+        
+    }
+}
