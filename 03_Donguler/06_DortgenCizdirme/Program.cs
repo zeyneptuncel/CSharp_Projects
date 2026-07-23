33209f8a4c0f06fd0c594aabcc872733d2c52cc0
Zeynep <zeynepqtuncel@gmail.com>
2026-07-23 15:37:51 +0300

    Dörtgen çizen program yazıldı

---
+++ 03_Donguler/06_DortgenCizdirme/Program.cs
@@ -0,0 +1,33 @@
+﻿// iki kenar uzunlugu girilerek dörtgen çizen prgram
+/* ORNEK CIKTI (5,5)
+    -----
+    |ooo|
+    |ooo|
+    |ooo|
+    -----   */
+Console.Write("1. kenar uzunluğunu giriniz: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. kenar uzunluğunu giriniz: ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= k; i++)
+{
+    if (i==1 || i==k)
+    {
+        for (int j = 1; j <= l; j++)
+        {
+            System.Console.Write("-");
+        }
+    }
+    else
+    {
+        System.Console.Write("|");
+        for (int o = 1; o <= l-2 ; o++)
+        {
+            System.Console.Write("o");
+        }
+        System.Console.Write("|");
+    }
+    System.Console.WriteLine();
+}
