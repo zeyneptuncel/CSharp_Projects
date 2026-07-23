4d7a97009fad83cb3111a8c09ff7b0b2229e46f7
Zeynep <zeynepqtuncel@gmail.com>
2026-07-23 14:50:14 +0300

    Sayı tahmin oyunu yapıldı

---
+++ 03_Donguler/04_SayiTahminEtmeOyunu/Program.cs
@@ -0,0 +1,31 @@
+﻿// 3 hak verilen rakam tahmin etme uygulaması
+int hak=3;
+Random rastgele = new Random();
+int number=rastgele.Next(10);
+System.Console.Write("Rakamı tahmin ediniz: ");
+while(hak!=0)
+{
+    int tahmin=Convert.ToInt32(Console.ReadLine());
+    hak--;
+    if (tahmin==number)
+    {
+        System.Console.Write("Tebrikler, Bildiniz.");
+        break;
+    }
+    else if (number>tahmin)
+    {
+        System.Console.Write("Daha büyük bir sayı giriniz: ");
+    }
+    else
+    {
+        System.Console.Write("Daha küçük bir sayı giriniz: ");
+    }
+    if (hak==0)
+    {
+        System.Console.Write($"Mallesef hakkınız kalmadı. Sayı: {number}"); 
+    } 
+    
+}
+
+
+
